9e91335034a3fc489f024c8727aa4e286a83b8c3
bololo7 <101951542+bololo7@users.noreply.github.com>
2023-10-29 21:08:15 +0100

    Message display

--- Scrimba/BlackJack/index.js
+++ Scrimba/BlackJack/index.js
@@ -5,16 +5,18 @@ let hasBlackjack = false;
 let isAlive = true;
 let message = "";
 
-if(sum <= 20){
-    message = "Do you want to draw a new card ?";
+let messageEL = document.getElementById("message-el");
+function startGame(){
+    if(sum <= 20){
+        message = "Do you want to draw a new card ?";
+    }
+    else if (sum ===21) {
+        message = "Blackjack !!";
+        hasBlackjack = true;
+    }
+    else{
+        message = "You lost !!";
+        isAlive = false;
+    }
+    messageEL.textContent = message;
 }
-else if (sum ===21) {
-    message = "Blackjack !!";
-    hasBlackjack = true;
-}
-else{
-    message = "You lost !!";
-    isAlive = false;
-}
-
-console.log(message);
