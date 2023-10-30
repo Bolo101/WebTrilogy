9ae226f8d939544f34d9ed884f039085419b0956
bololo7 <101951542+bololo7@users.noreply.github.com>
2023-10-30 22:23:01 +0100

    New card button implemented, displays cards & sum

--- Scrimba/BlackJack/index.js
+++ Scrimba/BlackJack/index.js
@@ -1,12 +1,23 @@
-let firstCard = 6;
-let secondCard = 9;
-let sum = firstCard + secondCard + 6;
+let firstCard = 10;
+let secondCard = 4;
+let cards = [firstCard,secondCard];
+let sum = firstCard + secondCard ;
 let hasBlackjack = false;
 let isAlive = true;
 let message = "";
 
 let messageEL = document.getElementById("message-el");
+//let sumEL = document.getElementById("sum-el");
+//Can also use querySelector using its css selector (# for an id; . for a class)
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.getElementById("cards-el");
+
 function startGame(){
+    renderGame();
+}
+function renderGame(){
+    cardsEl.textContent = "Cards : " + cards[0] + " " + cards[1];
+    sumEl.textContent = "Sum : " + sum;
     if(sum <= 20){
         message = "Do you want to draw a new card ?";
     }
@@ -19,4 +30,11 @@ function startGame(){
         isAlive = false;
     }
     messageEL.textContent = message;
+}
+
+function newCard(){
+    console.log("Drawing a new card from the deck!");
+    let card = 5;
+    sum += card;
+    renderGame(); 
 }
